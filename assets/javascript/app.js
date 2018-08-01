eea3833ee14e17f0ae4ac63a3664a303b2fe96e9
TinTDang <tin2dang@gmail.com>
2018-08-01 17:11:29 -0500

    created temp trivia

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,20 @@
+//Variables here
+var trivia = [{
+    question: "How would you write 'blahblah' in jQuery",
+    choices: ["$('mov')", "$('vow')", "$(she)", "$('#clash')"],
+    answer: 1,
+}]
+
+
+
+
+//Functions here
 $("#results").hide();
 
+//Starts the game with the button click
 $("#startButton").on("click", function(){
     $("#results").show();
     $("#startGame").hide();
 });
+
+
