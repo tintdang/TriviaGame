5fd8530a632baf67b18749653ba15c97f667f47b
TinTDang <tin2dang@gmail.com>
2018-08-01 19:49:56 -0500

    added timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,62 @@
 var trivia = [{
     question: "How would you write 'blahblah' in jQuery",
     choices: ["$('mov')", "$('vow')", "$(she)", "$('#clash')"],
-    answer: 1,
+    answer: 4,
+},{
+    question: "How would you write 'blahblah' in jQuery",
+    choices: ["$('mov')", "$('vow')", "$(she)", "$('#clash')"],
+    answer: 4,
+},{
+    question: "How would you write 'blahblah' in jQuery",
+    choices: ["$('mov')", "$('vow')", "$(she)", "$('#clash')"],
+    answer: 4,
+},{
+    question: "How would you write 'blahblah' in jQuery",
+    choices: ["$('mov')", "$('vow')", "$(she)", "$('#clash')"],
+    answer: 4,
+},{
+    question: "How would you write 'blahblah' in jQuery",
+    choices: ["$('mov')", "$('vow')", "$(she)", "$('#clash')"],
+    answer: 4,
+},{
+    question: "How would you write 'blahblah' in jQuery",
+    choices: ["$('mov')", "$('vow')", "$(she)", "$('#clash')"],
+    answer: 4,
+},{
+    question: "How would you write 'blahblah' in jQuery",
+    choices: ["$('mov')", "$('vow')", "$(she)", "$('#clash')"],
+    answer: 4,
+},{
+    question: "How would you write 'blahblah' in jQuery",
+    choices: ["$('mov')", "$('vow')", "$(she)", "$('#clash')"],
+    answer: 4,
 }]
+
+var intervalId;
+
+//create timer
+var timer = {
+    time: 10,
+    reset: function(){
+        $("#timer").text(10);
+        time: 10;
+    },
+    start: function(){
+        intervalId = setInterval(timer.count, 1000);
+        if (time === 0){
+
+        }
+    },
+    stop: function(){
+        
+    },
+    countdown: function(){ //displays the 
+        var count = timer.time--;
+        $("timer").text(count)
+    }
+
+}
+
 
 
 
