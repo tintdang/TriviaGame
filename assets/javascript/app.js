e142337984381e5c20803350adc02fb6a55f8d30
TDang <tin2dang@gmail.com>
2018-08-02 17:22:25 -0500

    Timers Work!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,33 @@
 //Variables here
-var trivia = {
-    question1: {
+var trivia = [
+    {
+        question: "How would you select an element in jQuery?",
+        choices: ["$('mov')", "$('vow')", "$(she)", "$('#clash')"],
+        answer: "D",
+    }, {
+        question: "How would you write 'blahblah' in jQuery",
+        choices: ["$('mov')", "$('vow')", "$(she)", "I am a test"],
+        answer: "A",
+    }, {
         question: "How would you write 'blahblah' in jQuery",
         choices: ["$('mov')", "$('vow')", "$(she)", "$('#clash')"],
-        answer: 4,
-    },
-    question2: {
-        question: "How would you write 'blahblah' in jQuery",
-        choices: ["$('mov')", "$('vow')", "$(she)", "$('#clash')"],
-        answer: 4,
-    },
-    question3: {
-        question: "How would you write 'blahblah' in jQuery",
-        choices: ["$('mov')", "$('vow')", "$(she)", "$('#clash')"],
-        answer: 4,
+        answer: "C",
     }
-}
-
+]
+var questionNumber = 0;
 var intervalId;
 var clockRunning = false;
+var intervalIdAnswer;
+var clockRunningAnswer = false;
+var correct = 0;
+var timedOut = 0;
+var incorrect = 0;
 
 //create timer
 var timer = {
-    time: 10,
+    time: 8,
     reset: function () {
-        clearInterval(intervalId);
-        clockRunning = false
-        timer.time = 10;
+        timer.time = 8;
     },
     start: function () {
         if (!clockRunning) {
@@ -35,22 +36,89 @@ var timer = {
         };
 
     },
+    stop: function() {
+        clearInterval(intervalId)
+        clockRunning = false;
+    },
     countdown: function () { //displays the 
         timer.time--;
         $("#timer").text(timer.time)
         if (timer.time == 0) {
-            timer.reset();
-        };
-    }
+            timedOut++;
+            $("#img").attr("src", "https://media.giphy.com/media/l378Aotu8uRBKYWHe/source.gif") //play this gif when they run out of time
+            answerTimer.start()
+            //they timed out so do a timed out here
+            questionNumber++
+            //run through next question
+        }
+    },
 
 }
 
+var answerTimer = {
+    time: 6,
+    start: function () {
+        if (!clockRunningAnswer) {
+            intervalIdAnswer = setInterval(answerTimer.countdown, 1000);
+            clockRunningAnswer = true;
+        };
+
+    },
+    stop: function() {
+        clearInterval(intervalIdAnswer)
+        clockRunningAnswer = false;
+    },
+    countdown: function () { //displays the 
+        answerTimer.time--;
+        $("#timer").text(answerTimer.time)
+        if (answerTimer.time == 0) {
+            //Run the next question
+            timer.reset();
+            $("#answerBox").hide();
+            $("#question").hide();
+            showQuestion();
+            answerTimer.stop();
+            answerTimer.time = 5;
+        }
+    },
+}
+
+function showQuestion(){
+    if(questionNumber === trivia.length){
+        //end the game please.
+        $("#questionBox").hide
+    }
+    else{
+    $("#question").text(trivia[questionNumber].question) //Shows current question
+    renderAnswers(); // Shows the answers through function renderAnswers
+    $("#question").show();
+    $("#answerBox").show();
+    timer.start();
+    // questionNumber++
+    // if they selected the right answer, reset interval and select 
+    // if timer.time === 0, show that they timed out
+    // else they selected the wrong answer
+    }
+}
+
+
+function renderAnswers(){
+    $("#A").text(trivia[questionNumber].choices[0]);
+    $("#B").text(trivia[questionNumber].choices[1]);
+    $("#C").text(trivia[questionNumber].choices[2]);
+    $("#D").text(trivia[questionNumber].choices[3]);
+}
+
+
+
 //Where my click functions would work
 window.onload = function () {
+
     // $("#A").on("click", timer.start) // Test function to see if timer works
 
 }
 
+// inside the question asking function and to look for when the game ends
 
 //Functions here
 $("#results").hide();
@@ -59,24 +127,55 @@ $("#results").hide();
 $("#startButton").on("click", function () {
     $("#results").show();
     $("#startGame").hide();
+    timer.start();
+    showQuestion();
 });
 
 //sets up answer buttons
-$(document).on("click", ".answer", function(){
+$(document).on("click", ".answer", function () {
     var answer = $(this).attr("option") //this calls the result of the answer
+    var correctAnswer = trivia[questionNumber].answer
     console.log(answer);
+    // hide answer box function
+    $("#answerBox").hide();
+    // clear question
+    $("#question").hide();
+
+    if (answer === correctAnswer){
+        correct++
+        $("#img").attr("src", "https://media.giphy.com/media/OhFuOJn2rbLsA/source.gif")
+        timer.stop();
+        answerTimer.start();
+        
+    }
+    else {
+        incorrect++
+        // $("#img").attr("src", "https://media.giphy.com/media/fV2nYFD3akDuTUgVhy/source.mp4")
+        timer.stop();
+        answerTimer.start();
+    }
+
+    questionNumber++
+
+    //then show answer box fast in both if else function
+
     //if answer matches with triviaanswer they get a point
     //if they don't answer the question in the amoutn of time, they get tracked of what questions they didn't answer
     //else they answered it wrong so keep talley of that
 });
 
-
-//Set up right answer box
-
+//Set up Question box
 
 
+//Set up answer box
+//Set up wrong answer
+//set up right answer
 
-//Set up wrong answer box
+
+//reveal questions right/ wrong/ timed out
+
+
+
 
 
 //Set up results at the end when they finish the trivia
