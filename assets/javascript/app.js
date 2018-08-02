3d3e354127c5897adc84e9ab74d3b51680f590fc
TinTDang <tin2dang@gmail.com>
2018-08-02 00:45:00 -0500

    added more pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,39 +18,42 @@ var trivia = {
 }
 
 var intervalId;
-    var clockRunning = false;
+var clockRunning = false;
 
-    //create timer
-    var timer = {
-        time: 10,
-        reset: function () {
-            clearInterval(intervalId);
-            clockRunning = false
-            timer.time = 10;
-        },
-        start: function () {
-            if (!clockRunning) {
-                intervalId = setInterval(timer.countdown, 1000);
-                clockRunning = true;
-            };
+//create timer
+var timer = {
+    time: 10,
+    reset: function () {
+        clearInterval(intervalId);
+        clockRunning = false
+        timer.time = 10;
+    },
+    start: function () {
+        if (!clockRunning) {
+            intervalId = setInterval(timer.countdown, 1000);
+            clockRunning = true;
+        };
 
-        },
-        countdown: function () { //displays the 
-            timer.time--;
-            $("#timer").text(timer.time)
-            if (timer.time == 0) {
-                timer.reset();
-            };
-        }
-
+    },
+    countdown: function () { //displays the 
+        timer.time--;
+        $("#timer").text(timer.time)
+        if (timer.time == 0) {
+            timer.reset();
+        };
     }
 
-$("#A").on("click", timer.start())
+}
+
+//Where my click functions would work
+window.onload = function () {
+    // $("#A").on("click", timer.start) // Test function to see if timer works
+
+}
 
 
-
-    //Functions here
-    $("#results").hide();
+//Functions here
+$("#results").hide();
 
 //Starts the game with the button click
 $("#startButton").on("click", function () {
@@ -58,4 +61,22 @@ $("#startButton").on("click", function () {
     $("#startGame").hide();
 });
 
+//sets up answer buttons
+$(document).on("click", ".answer", function(){
+    var answer = $(this).attr("option") //this calls the result of the answer
+    console.log(answer);
+    //if answer matches with triviaanswer they get a point
+    //if they don't answer the question in the amoutn of time, they get tracked of what questions they didn't answer
+    //else they answered it wrong so keep talley of that
+});
 
+
+//Set up right answer box
+
+
+
+
+//Set up wrong answer box
+
+
+//Set up results at the end when they finish the trivia
